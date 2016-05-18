9ee5e607cf47f0dd9bc154e1fd032754b5e18c75
metaxiii <lehchibi.gael@gmail.com>
2016-05-18 15:13:31 +0200

    Ajout de la page register, avec formulaire

--- PPE2/Production/Maquette/www/components/ui/register/ctrl.js
+++ PPE2/Production/Maquette/www/components/ui/register/ctrl.js
@@ -1,2 +1,7 @@
 export default function(){
+ const self = this;
+ 
+ self.user = {};
+ let countries = ['France', 'Great Britain', 'Deutschland', 'Luxembourg' , 'Italy', 'Espana', 'Portugal'];
+ self.countries = countries;
 };
