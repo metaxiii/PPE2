acb605c1fe0c0d22492ae445e9f027ab1858be6f
Charles BARDIN <bardincharles@gmail.com>
2016-05-21 20:30:58 +0200

    Modify booking view

--- PPE2/Production/Maquette/www/components/ui/booking/ctrl.js
+++ PPE2/Production/Maquette/www/components/ui/booking/ctrl.js
@@ -3,9 +3,34 @@ import conviv from './../../../img/conviv.jpg' ;
 import reunion from './../../../img/reunion.jpg' ; 
 
 export default function(){
-	const self = this ; 
+	const self = this ;
 	
 	self.conviv = conviv ;
 	self.reunion = reunion ; 
 	self.amphitheatre = amphitheatre ;
+	
+	// const showBookingDialog = (room) => {
+	// 	let dialogController = ['$mdDialog', 'room', function($mdDialog, room) {
+	// 		self.room = room;
+			
+	// 		self.cancel = function(){
+	// 			$mdDialog.hide();
+	// 		};
+	// 	}]
+	// 	$mdDialog.show({
+	// 		template: 	'<md-dialog flex>' +
+	// 						'<md-dialog-content layout="column">' +
+	// 							'<h3>{{room}}</h3>' +
+	// 							'<label>Date de réservation</label>' +
+	// 							'<md-datepicker ng-model="bookingDate"></md-datepicker>' +
+	// 						'</md-dialog-content>' +
+	// 					'</md-dialog>',
+	// 		locals: {
+	// 			room: room
+	// 		},
+	// 		controller: dialogController
+	// 	})
+	// };
+	
+	// self.showBookingDialog = showBookingDialog;
 };
