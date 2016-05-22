27c67f3de855933228b306e12d30f68c58c8859a
kaosse <machacekloic@yahoo.fr>
2016-05-22 20:38:13 +0200

    Script js password / mail

--- PPE2/Production/Maquette/www/components/ui/register/ctrl.js
+++ PPE2/Production/Maquette/www/components/ui/register/ctrl.js
@@ -2,33 +2,42 @@ import sha256 from 'crypto-js/sha256';
 
 export default ['AuthService', '$state', function(AuthService, $state){
   const self = this;
+ 
   
   if(AuthService.getAuth())
 		$state.go('index.info');
 
   const register = function(){
-    self.registerError = '';
-
-    // TODO: add verifications
-    let newUser = {
-      nickname: self.user.nickname,
-      name: self.user.name,
-      mail: self.user.mail,
-      password: '' + sha256(self.user.password),
-      adress: {
-        street: '' + self.streetNumber + ' ' + self.streetName,
-        zip: self.user.adress.zip,
-        town: self.user.adress.town,
-        country: self.user.adress.country
-      }
-    }
-    
-    AuthService.registerUser(newUser)
-      .then(function(){
-        $state.go('index.booking');
-      }, function(err){
-        self.registerError = err;
-      });
+	self.registerError = '';
+	
+	if (self.user.password !== self.passwordVerif){
+		self.registerError = 'mot de passe incorrect';
+	}
+	else if(self.user.mail !== self.mailVerif){
+		self.registerError = 'email incorrect';
+	}
+	else {
+		// TODO: add verifications
+		let newUser = {
+		  nickname: self.user.nickname,
+		  name: self.user.name,
+		  mail: self.user.mail,
+		  password: '' + sha256(self.user.password),
+		  adress: {
+			street: '' + self.streetNumber + ' ' + self.streetName,
+			zip: self.user.adress.zip,
+			town: self.user.adress.town,
+			country: self.user.adress.country
+		  }
+		}
+		
+		AuthService.registerUser(newUser)
+		  .then(function(){
+			$state.go('index.booking');
+		  }, function(err){
+			self.registerError = err;
+		  });
+	}
   }
   
   const countries = ['France', 'Great Britain', 'Deutschland', 'Luxembourg' , 'Italy', 'Espana', 'Portugal'];
